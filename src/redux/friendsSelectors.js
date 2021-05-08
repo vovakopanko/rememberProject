68a2d4012fb5d58922c3583214c7285afa719f90
vovakopanko <vovakopanko@mail.ru>
2021-05-09 00:31:33 +0300

    Append selector, reselector library in Friends Page

--- src/redux/friendsSelectors.js
+++ src/redux/friendsSelectors.js
@@ -1,27 +1,51 @@
-export const getFriends = (state) => {
+import { createSelector } from "reselect";
+
+export const getFriendsSelector = (state) => {
   return state.friendsPage.userFriends;
 };
 
-export const getTotalFriends = (state) => {
+export const getFriends = createSelector(getFriendsSelector, (userFriends) =>  { 
+  return userFriends})
+  
+
+export const getTotalFriendsSelector = (state) => {
   return state.friendsPage.totalUsersCount;
 };
 
-export const getPageSize = (state) => {
+export const getTotalFriends = createSelector(getTotalFriendsSelector, (totalUsersCount) =>  { 
+  return totalUsersCount})
+
+export const getPageSizeSelector = (state) => {
   return state.friendsPage.pageSize;
 };
 
-export const getCurrentPage = (state) => {
+export const getPageSize = createSelector(getPageSizeSelector, (pageSize) =>  { 
+  return pageSize})
+
+export const getCurrentPageSelector = (state) => {
   return state.friendsPage.currentPage;
 };
 
-export const getIsFetching = (state) => {
+export const getCurrentPage = createSelector(getCurrentPageSelector, (currentPage) =>  { 
+  return currentPage})
+
+export const getIsFetchingSelector = (state) => {
   return state.friendsPage.isFetching;
 };
 
-export const getIsButtonLock = (state) => {
+export const getIsFetching = createSelector(getIsFetchingSelector, (isFetching) =>  { 
+  return isFetching})
+
+export const getIsButtonLockSelector = (state) => {
   return state.friendsPage.isButtonLock;
 };
 
-export const getIsAuth = (state) => {
+export const getIsButtonLock = createSelector(getIsButtonLockSelector, (isButtonLock) =>  { 
+  return isButtonLock})
+
+export const getIsAuthSelector = (state) => {
   return state.auth.isAuth;
 };
+
+export const getIsAuth = createSelector(getIsAuthSelector, (isAuth) =>  { 
+  return isAuth})
